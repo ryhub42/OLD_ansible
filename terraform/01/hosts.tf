2db7dc28170689f15e303aafe40228915821a55c
Rajani y <ryhub42@gmail.com>
2019-11-30 12:12:27 -0500

    6

--- terraform/01/hosts.tf
+++ terraform/01/hosts.tf
@@ -1,4 +1,4 @@
 resource "local_file" "foo" {
-  content     = "${aws_instance.web.public_ip[0]}\n${aws_instance.web.public_ip[1]}\n"
+  content     = "${aws_instance.web.*.public_ip[0]}\n${aws_instance.web.*.public_ip[1]}\n"
   filename = "/home/centos/ansible/hosts"
 }
