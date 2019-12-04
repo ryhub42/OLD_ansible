ca8849fc8bb585f17cfc88eabef41ae441ac7546
Rajani y <ryhub42@gmail.com>
2019-12-04 11:28:35 -0500

    Set Host_key to false

--- terraform/01/ec2.tf
+++ terraform/01/ec2.tf
@@ -1,8 +1,9 @@
 resource "aws_instance" "web" {
-  count         = 2
-  ami           = "ami-02383135f96b2293e"
-  instance_type = "t2.micro"
-  key_name      = "devops"
+  count                    = 2
+  ami                      = "ami-02383135f96b2293e"
+  instance_type            = "t2.micro"
+  key_name                 = "devops"
+  vpc_security_group_ids   = ["sg-08bed8525f85d783e","sg-0f8ef2eb8f766f7d1"]
 
   tags = {
     Name = "Ansible-Node-${count.index+1}"
