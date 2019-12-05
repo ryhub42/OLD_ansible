4beec9f038a2176b70710602d109921e5aba190d
Rajani y <ryhub42@gmail.com>
2019-12-05 15:28:30 -0500

    Set Host_key to false

--- sample-yaml/email.sh
+++ sample-yaml/email.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
 read -p 'Enter batch no: 'batchno
+VALUE=$(cat courses.yaml | yq . | jq ".b${batchno}")
+if [ $VALUE = "null" ]; then
+  echo "Given batch number doesn't exist"
+  exit 1
+fi
