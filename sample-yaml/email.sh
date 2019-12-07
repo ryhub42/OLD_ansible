8e7b5a4e876ce4106ecc8f71444edc2c9ffa35df
Rajani y <ryhub42@gmail.com>
2019-12-07 13:27:40 -0500

    updated2

--- sample-yaml/email.sh
+++ sample-yaml/email.sh
@@ -7,10 +7,12 @@ if [ "$VALUE" = "null" ]; then
   exit 1
 fi
 
-##NAMES=('cat courses.yml | yq . | jq '.b42.students[].name'|xargs')
-##EMAILS=('cat courses.yml | yq . | jq '.b42.students[].email'|xargs')
+NAMES=('cat courses.yml | yq . | jq '.b42.students[].name'|xargs')
+EMAILS=('cat courses.yml | yq . | jq '.b42.students[].email'|xargs')
 
-##VALUE_NO=$(echo ${#NAMES[*]})
+VALUE_NO=$(echo ${#NAMES[*]})
 
-##while [ \
-##\
+while [  $VALUE_NO -gt 0 ]; do
+  VALUE_NO=$(($VALUE_NO-1))
+  echo -e "Hello ${NAMES{$VALUE_NO}}, I am sending class info to ${EMAILS[$VALUE_NO]}"
+done
