b593fdf41ffd79cd8c89891022025f604f5c850b
Rajani y <ryhub42@gmail.com>
2019-12-07 14:09:20 -0500

    updated4

--- sample-yaml/email.sh
+++ sample-yaml/email.sh
@@ -3,19 +3,19 @@
 read -p 'Enter batch no: ' batch_no
 VALUE=$(cat courses.yml | yq . | jq ".b${batch_no}")
 if [ "$VALUE" = "null" ]; then
-  echo "Given batch number doesnot exist"
+  echo "Given batch number doest exist"
   exit 1
 fi
 
-NAMES=('cat courses.yml | yq . | jq ".b${batch_no}.students[].name"|xargs')
-EMAILS=('cat courses.yml | yq . | jq .b${batch_no}.students[].email|xargs')
-TRAINER_NAME=$(cat courses.yml | yq . | jq .b${batch_no}.trainer_name|xargs)
-COURSE_NAME=$(cat courses.yml | yq . | jq .b${batch_no}.course_name|xargs)
-COURSE_TIME=$(cat courses.yml | yq . | jq ".b${batch_no}..timing"|xargs)
+NAMES=(`cat courses.yml | yq . | jq ".b${batch_no}.students[].name"|xargs`)
+EMAILS=(`cat courses.yml | yq . | jq .b${batch_no}.students[].email|xargs`)
+TRAINER_NAME=$(cat courses.yml  | yq . | jq .b${batch_no}.trainer_name |xargs)
+COURSE_NAME=$(cat courses.yml  | yq . | jq .b${batch_no}.course_name|xargs)
+COURSE_TIME=$(cat courses.yml  | yq . | jq ".b${batch_no}.timing"|xargs)
 
 VALUE_NO=$(echo ${#NAMES[*]})
 
 while [ $VALUE_NO -gt 0 ]; do
   VALUE_NO=$(($VALUE_NO-1))
-  echo -e "Hello ${NAMES[$VALUE_NO]}, A new batch of $COURSE_NAME @ $COURSE_TIME by $TRAINER_NAME will being from monday and sending class info to ${EMAILS[$VALUE_NO]}"
+  echo -e "Hello ${NAMES[$VALUE_NO]}, A new batch of $COURSE_NAME @$COURSE_TIME by $TRAINER_NAME is getting started from Monday, I am sending an email to ${EMAILS[$VALUE_NO]}, Please respond if you have any queries."
 done
