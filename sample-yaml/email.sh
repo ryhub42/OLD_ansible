f1b5d800186b16a1da506957d17ff571a40596f9
Rajani y <ryhub42@gmail.com>
2019-12-07 13:16:36 -0500

    updated1

--- sample-yaml/email.sh
+++ sample-yaml/email.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 read -p 'Enter batch no: ' batchno
-VALUE=$(cat courses.yaml | yq . | jq ".b${batchno}")
+VALUE=$(cat courses.yml | yq . | jq ".b${batchno}")
 if [ $VALUE = "null" ]; then
   echo "Given batch number doesn't exist"
   exit 1
