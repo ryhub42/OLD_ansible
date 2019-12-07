78e41b1dd011999ec241e43494450e4fd8556a7b
Rajani y <ryhub42@gmail.com>
2019-12-07 13:36:55 -0500

    updated3

--- sample-yaml/email.sh
+++ sample-yaml/email.sh
@@ -9,10 +9,13 @@ fi
 
 NAMES=('cat courses.yml | yq . | jq '.b42.students[].name'|xargs')
 EMAILS=('cat courses.yml | yq . | jq '.b42.students[].email'|xargs')
+TRAINER_NAME
+COURSE_NAME
+COURSE_TIME
 
 VALUE_NO=$(echo ${#NAMES[*]})
 
 while [  $VALUE_NO -gt 0 ]; do
   VALUE_NO=$(($VALUE_NO-1))
-  echo -e "Hello ${NAMES[$VALUE_NO]}, I am sending class info to ${EMAILS[$VALUE_NO]}"
+  echo -e "Hello ${NAMES[$VALUE_NO]}, A new batch of $COURSE_NAME @ $COURSE_TIME by $TRAINER_NAME will being from monday and sending class info to ${EMAILS[$VALUE_NO]}"
 done
